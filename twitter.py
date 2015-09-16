ed52f0693e3c444ca7ca7ed77e6b1ec736e403db
Lawrence Liu <suo.lawrence.liu@gmail.com>
2015-09-15 23:21:56 -0400

    updates

--- twitter.py
+++ twitter.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, session, url_for
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 	return 'Hello, world'
 
+@app.route('/about')
+def about():
+	return 'Lawrence'
 
 if __name__ == "__main__":
-	app.run()
+	port  = int(os.environ.get("PORT", 5000)
+	app.run(host = '0.0.0.0', port = port )
